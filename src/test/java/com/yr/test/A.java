764ef67158a52d47c9ba2a507a97a1027372d727
= <745065278@qq.com>
2021-07-14 10:34:02 +0800

    123123

---
+++ src/test/java/com/yr/test/A.java
@@ -0,0 +1,26 @@
+package com.yr.test;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class A {
+
+	@Test
+	public void aa()
+	{
+		assertEquals(1, 1);
+	}
+	
+	@Test
+	public void bb()
+	{
+		assertEquals(1, 1);
+	}
+	
+	
+	@Test
+	public void cc()
+	{
+		assertEquals(1, 2);
+	}
+}
